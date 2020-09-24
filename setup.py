67472a0e56fb13949f62246c1435b0cbb094ad21
Fadi Baskharon <fadi.baskharon@ericsson.com>
2020-09-25 01:00:16 +0200

    extras

--- setup.py
+++ setup.py
@@ -19,22 +19,20 @@ setuptools.setup(
         'Keras==2.3.1',
         'Keras-Applications==1.0.8',
         'Keras-Preprocessing==1.1.0',
+        'matplotlib>=3.2.1',
         'numpy>=1.18.2',
-        'pandas>=1.0.3',
+        'pandas>=1.0.0',
+        'scikit-learn>=0.22.2.post1',
         'scipy>=1.3.1',
-        'tensorboard>=2.0.1',
-        'tensorflow==2.0.1',
-        'tensorflow-estimator==2.0.1',
+        'seaborn>=0.10.0',
         'wtte>=1.1.1',
-
     ],
-    extras_requires={
-        "dev": [
-            'matplotlib>=3.2.1',
-            'ipykernel>=5.3.4',
-            'seaborn>=0.10.0',
-            'pytest>=6.0.1',
-            'pytest-docs>=0.1.0',
+    extras_require={
+        [
+            'pylint==2.6.0',
+            'twine>=3.2.0',
+            'pytest==6.0.1',
+            'pytest-docs==0.1.0',
         ]
     },
     classifiers=[
