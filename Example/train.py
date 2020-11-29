61151294e7ff773fa9c9575599f2f5050c02d892
Fadi Baskharon <fadi.baskharon@ericsson.com>
2020-11-29 13:55:50 +0200

    remove dev

--- Example/train.py
+++ Example/train.py
@@ -13,27 +13,40 @@ a = os.path.join(path, 'helpdesk.csv')
 b = os.path.join(path, 'bpi_12_w.csv')
 c = os.path.join(path, 'bpi_12_w_no_repeat.csv')
 d = os.path.join(path, 'env_permit.csv')
+e = os.path.join(path, 'bpic13_all.csv')
 
 a = pd.read_csv(a)
 b = pd.read_csv(b)
 c = pd.read_csv(c)
 d = pd.read_csv(d)
+e = pd.read_csv(e)
 
+end_event_dict = {
+    'a': 6,
+    'b': 6,
+    'c': 6,
+    'd': 6,
+    'e': ['Completed+Closed', 'Completed+In Call', 'Completed-Closed',
+          'Completed+Resolved', 'Completed+Cancelled', 'Completed-Cancelled'],
+}
 df_dict = {
     'a': a,
     'b': b,
     'c': c,
-    'd': d
+    'd': d,
+    'e': e,
 }
 range_dict = {
     'a': range(2, 8, 1),
     'b': range(2, 22, 2),
     'c': range(2, 12, 2),
-    'd': range(2, 22, 2)
+    'd': range(2, 22, 2),
+    'e': range(2, 22, 2),
 }
 
 
-def grid_search(dataset, res, censored, cen_per, fit_type, transform):
+def grid_search(dataset, res, censored, extra_censored, end_event_list, 
+                fit_type, transform):
 
     cols = ["prefix", "Layer_Size", "MAE", "unique_pred", "train_size",
             "val_size", "test_size", "Censored %", "fit_time"]
@@ -41,15 +54,16 @@ def grid_search(dataset, res, censored, cen_per, fit_type, transform):
     for prefix in range_dict[dataset]:
         print("\ndataset:", dataset, "\tprefix:", prefix)
         t2e_obj = t2e(
-            df_dict[dataset],
+            dataset=df_dict[dataset],
             prefix=prefix,
             resolution=res,
-            censored=censored,
-            cen_prc=cen_per,
+            extra_censored=extra_censored,
             fit_type=fit_type,
-            transform=transform
+            transform=transform,
+            end_event_list=end_event_list,
+            censored=censored
         )
-        t2e_obj.preprocess_dev()
+        t2e_obj.preprocess()
         X_train, X_test, X_val, y_train, y_test, y_val,\
             len_train, len_val, len_test = t2e_obj.smart_split(
                                                             train_prc=0.7,
@@ -73,59 +87,21 @@ def grid_search(dataset, res, censored, cen_per, fit_type, transform):
 
 
 def main():
-    for dataset in ['a']:  # ['b', 'c', 'd']:
-
-        #   Experiment #1: T2E Un-censored No transformation
-        # for cen_prc in [0]:
-
-        #     mae_path = 'output/maes/t2e/'+str(cen_prc) + '/'
-        #     check_dir(mae_path)
-        #     print('### Censored percentage:', cen_prc/100.0)
-        #     grid_results = grid_search(
-        #                         dataset=dataset,
-        #                         res='s',
-        #                         censored=False,
-        #                         cen_per=cen_prc/100.0,
-        #                         fit_type='t2e',
-        #                         transform=False
-        #                 )
-        #     pickle.dump(grid_results, open(mae_path + 't2e_'
-        #                                    + dataset + '_GRU.pkl', 'wb'))
-
-        # Experiment #2: T2E Un-censored transform
-        # for cen_prc in [0]:
-
-        #     mae_path = 'output/maes/t2e_transform/'+str(cen_prc) + '/'
-        #     check_dir(mae_path)
-        #     print('### Censored percentage:', cen_prc/100.0)
-        #     grid_results = grid_search(
-        #                         dataset=dataset,
-        #                         res='s',
-        #                         censored=False,
-        #                         cen_per=cen_prc/100.0,
-        #                         fit_type='t2e',
-        #                         transform=True
-        #                 )
-        #     pickle.dump(grid_results, open(mae_path + 't2e_'
-        #                                    + dataset + '_GRU.pkl', 'wb'))
-
-        #    Experiment #3: T2E Censored transform
-        for cen_prc in [50]:
-            mae_path = 'output/maes/t2e_transform/'+str(cen_prc) + '/'
-            print('saving  ==> ', mae_path)
-            check_dir(mae_path)
-
-            print('### Censored percentage:', cen_prc/100.0)
-            grid_results = grid_search(
-                                dataset=dataset,
-                                res='s',
-                                censored=True,
-                                cen_per=cen_prc/100.0,
-                                fit_type='t2e',
-                                transform=True
-                        )
-            pickle.dump(grid_results, open(mae_path + 't2e_'
-                                           + dataset + '_GRU.pkl', 'wb'))
+    for dataset in ['e']:  # ['b', 'c', 'd']:
+        mae_path = '../output/maes/low_censored_transform/'
+        check_dir(mae_path)
+        print('saving  ==> ', mae_path)
+        grid_results = grid_search(
+                            dataset=dataset,
+                            res='s',
+                            extra_censored=0,
+                            end_event_list=end_event_dict[dataset],
+                            fit_type='t2e',
+                            transform=True,
+                            censored=True,
+        )
+        pickle.dump(grid_results, open(mae_path + dataset +
+                    '_GRU.pkl', 'wb'))
 
 
 if __name__ == "__main__":
--- cycle_prediction/t2e.py
+++ cycle_prediction/t2e.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 import time
 import math
+import logging
+import random
 from math import ceil
-from collections import Counter
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, LSTM, GRU
 from tensorflow.keras.layers import BatchNormalization, Lambda, Input
@@ -23,9 +24,18 @@ from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 
 sd = 100
+random.seed(sd)
 np.random.seed(sd)
 tf.random.set_seed(sd)
 
+save_logs = False
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+logger = logging.getLogger("log")
+logging.basicConfig(
+        level=logging.DEBUG,
+        format=LOG_FORMAT,
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
 #################################################
 #################################################
 
@@ -51,11 +61,11 @@ class t2e:
         dataset,
         prefix,
         resolution,
-        censored=False,
-        cen_prc=0,
+        extra_censored=0,
         end_event_list=[],
         transform=False,
         fit_type='t2e',
+        censored=True
     ):
         """Initializes the t2e object with the desired setup.
 
@@ -64,22 +74,23 @@ class t2e:
             prefix (int): Number of history prefixes to train with.
             resolution (str): remaining time resolution
                 {'s': 'seconds', 'h':'hours', 'd':'days'}
-            censored (bool): Whether to randomely censor some traces.
-                Used only for development (default:False)
-            cen_prc (float): 0.0 -> 1.0, represents the percentage of
-                censored traces to generate (used ONLY for development)
+            extra_censored (float): Number of censored traces to create from
+                complete traces.
             end_event_list (obj): list of (int) containing the process's
                 possible end events
-            fit_type (str): t2e (default) => for furture development.
             transform (bool): Transform the output to a new space where
                 it is less biased toward short traces.
+            fit_type (str): t2e (default) => for furture development.
+            censored (bool): Whether to use/ignore the censored traces
+                (if found).
 
         """
         self.dataset = dataset
         self.prefix = prefix
+        self.extra_censored = extra_censored
         self.censored = censored
-        self.cen_prc = cen_prc
         self.censored_cases = []
+        self.observed_cases = []
         self.all_cases = []
         self.len_censored = 0
         self.len_observed = 0
@@ -141,49 +152,83 @@ class t2e:
             +--------------------+-----------+-------------------------------+
 
         """
-        # Retrieve all sequences with their records count
+        # Safe condiftions
         if self.end_event_list == []:
             raise ValueError(
-             "end_event_list should not be empty when using self.preprocess()"
+                "end_event_list should not be empty"
             )
-        self.dataset.sort_values(
-            ["CaseID", "CompleteTimestamp"], ascending=True)
-        case_counts = self.dataset.groupby(["CaseID"]).count()
-
-        # Only keep sequences that has at least prefix + 1 records
-        to_drop = list(
-            case_counts.loc[case_counts["ActivityID"] <= self.prefix].index)
-        self.dataset = self.dataset.loc[~self.dataset["CaseID"].isin(
-            to_drop)].reset_index(drop=True)
-        # Store all cases length
-        self.all_cases = self.dataset["CaseID"].unique()
-        print('No. of cases', len(self.all_cases))
-        # Create censored identifier based on end_event_list
-        cen_dict = self.dataset.drop_duplicates('CaseID', keep='last')[
-                            ['CaseID', 'ActivityID']]\
-                       .set_index('CaseID')\
-                       .to_dict()['ActivityID']
-        for k, v in cen_dict.items():
-            if v in self.end_event_list:
-                cen_dict[k] = 1
-            else:
-                cen_dict[k] = 0
-        # Assign the censorship state
-        self.dataset['U'] = self.dataset['CaseID'].map(cen_dict)
-        self.censored_cases = list(
-            np.unique(self.dataset['CaseID']
-                          .loc[self.dataset['U'] == 0]
-                          .values))
-        # process time features
+        logger.info("Prefix = %d", self.prefix)
+        # Set datatime colum
         self.dataset.CompleteTimestamp = pd.to_datetime(
             self.dataset.CompleteTimestamp)
+        self.dataset.sort_values(
+            ["CaseID", "CompleteTimestamp"], ascending=True)
+        logger.info('Total    cases: %d', self.dataset["CaseID"].nunique())
+        # Retrieve all sequences with their records count
+        # Only keep sequences that has at least prefix + 1 records
+        case_counts = self.dataset.groupby(["CaseID"]).count()
+        below_prefix = list(
+            case_counts.loc[case_counts["ActivityID"] <= self.prefix].index)
+        self.dataset = self.dataset.loc[~self.dataset["CaseID"].isin(
+            below_prefix)].reset_index(drop=True)
+
+        # Store all cases length
+        self.all_cases = self.dataset["CaseID"].unique()
+        logger.info('Prefix   cases: %d', len(self.all_cases))
+
+        # Create censorship dictionary based on end_event_list
+        self.cen_dict = self.dataset.drop_duplicates('CaseID', keep='last')[
+                                        ['CaseID', 'ActivityID']]\
+                                    .set_index('CaseID')\
+                                    .to_dict()['ActivityID']
+        for k, v in self.cen_dict.items():
+            if v in self.end_event_list:
+                self.cen_dict[k] = 1
+            else:
+                self.cen_dict[k] = 0
+        observed_cases = [k for k, v in self.cen_dict.items() if v == 1]
+        random.shuffle(observed_cases)
+        # Add extra censored cases
+        i = 0
+        for case in observed_cases:
+            if i < self.extra_censored:
+                tmp_df = self.dataset.loc[self.dataset['CaseID'] == case]
+                if len(tmp_df) >= self.prefix + 2:
+                    i += 1
+                    last_index = tmp_df.index[-1]
+                    # possible trims
+                    x = np.arange(1, len(tmp_df) - self.prefix)
+                    random_censor = np.random.choice(x, 1, replace=False)[0]
+                    drop_idx = []
+                    for j in range(random_censor):
+                        drop_idx.append(last_index - j)
+                    self.dataset['CaseID'] = self.dataset['CaseID']\
+                                                 .drop(drop_idx, axis=0)
+                    self.cen_dict[case] = 0
+                else:
+                    continue
+            else:
+                break
+
+        # Assign the censorship state
+        self.dataset['CaseID'].reset_index(drop=True, inplace=True)
+        self.dataset['U'] = self.dataset['CaseID'].map(self.cen_dict)
+        self.censored_cases = [k for k, v in self.cen_dict.items()
+                               if int(v) == 0]
+        self.observed_cases = [k for k, v in self.cen_dict.items()
+                               if int(v) == 1]
+        logger.info("Censored cases: %d", len(self.censored_cases))
+        logger.info("Observed cases: %d", len(self.observed_cases))
+        # process time features
         self.dataset = self.dataset.groupby("CaseID").apply(
-            lambda case: self.__time_features(case))
+                        lambda case: self.__time_features(case))
         self.dataset.fvt1.fillna(0, inplace=True)
         # process activity features
         dummy = pd.get_dummies(
-            self.dataset["ActivityID"], prefix="ActivityID", drop_first=True)
+            self.dataset["ActivityID"], prefix="ActivityID", drop_first=False)
         self.dataset = pd.concat([self.dataset, dummy], axis=1)
+
+        # Remove last step
         last_step = self.dataset.drop_duplicates(
             subset=["CaseID"], keep='last')["ActivityID"].index
         self.dataset = self.dataset.drop(
@@ -191,103 +236,9 @@ class t2e:
         # drop useless columns
         self.dataset = self.dataset.drop(
             ['CompleteTimestamp', 'T2E'], axis=1)
-        # Set counts
-        val_counts = Counter(cen_dict.values())
-        self.len_censored = val_counts[0]
-        self.len_observed = val_counts[1]
-
-    def preprocess_dev(self):
-        """This method is designed for developing purpose ONLY.
-
-        - Generate censored traces from the observed traces
-        - Feature engineering
-        - One-hot-encoding
-
-        Args:
-            __init__ Mandatory parameter:
-                - dataset
-                - prefix
-                - resolution
-                - censored
-                - cen_prc
-            __init__ Optional parameter:
-                - transform
-                - fit_type
-
-        Returns:
-            :obj:`self`: updated self.dataset to a pandas dataframe with the\
-                following format:
-
-            +--------------------+-----------+-------------------------------+
-            | Column             | Data type | Content                       |
-            +====================+===========+===============================+
-            | CaseID             | int       | Case identifier               |
-            +--------------------+-----------+-------------------------------+
-            | ActivityID         | int       | Activity identifier           |
-            +--------------------+-----------+-------------------------------+
-            | CompleteTimestamp  | datetime  | Timestamp of the event        |
-            +--------------------+-----------+-------------------------------+
-            | fvt1               | float     | delta time to the next event  |
-            +--------------------+-----------+-------------------------------+
-            | fvt2               | float     | hour since day start          |
-            +--------------------+-----------+-------------------------------+
-            | fvt3               | float     | hour since week start         |
-            +--------------------+-----------+-------------------------------+
-            | ActivityID_1       | int       | Activity in one-hot form      |
-            +--------------------+-----------+-------------------------------+
-            | ...                | ...       | ...                           |
-            +--------------------+-----------+-------------------------------+
-            | ActivityID_n       | ...       | ...                           |
-            +--------------------+-----------+-------------------------------+
-            | U                  | int       | 0/1 : censored/observed trace |
-            +--------------------+-----------+-------------------------------+
-            | T2E/D2E/S2E        | float     | Remaining time in seconds,    |
-            |                    |           | hours or days                 |
-            +--------------------+-----------+-------------------------------+
-
-        """
-        # Retrieve all sequences with their records count
-        self.dataset.sort_values(
-            ["CaseID", "CompleteTimestamp"], ascending=True)
-        case_counts = self.dataset.groupby(["CaseID"]).count()
-
-        # Only keep sequences that has at least prefix + 1 records
-        to_drop = list(
-            case_counts.loc[case_counts["ActivityID"] <= self.prefix].index)
-        self.dataset = self.dataset.loc[~self.dataset["CaseID"].isin(
-            to_drop)].reset_index(drop=True)
-        self.all_cases = self.dataset["CaseID"].unique()
-        print('all cases', len(self.all_cases))
-        # Generate censored data:
-        to_censor_from = list(
-            self.dataset
-                .groupby(["CaseID"])
-                .count()
-                .loc[case_counts["ActivityID"] > self.prefix+1].index
-                )
-        try:
-            np.random.seed(sd)
-            self.censored_cases = np.random.choice(to_censor_from, int(
-                len(self.all_cases)*self.cen_prc), replace=False)
-        except Exception:
-            self.censored_cases = np.array(to_censor_from)
-
-        print("first 10 censored cases", self.censored_cases[0:10])
-
-        self.dataset["U"] = ~self.dataset['CaseID'].isin(self.censored_cases)*1
-        self.dataset.CompleteTimestamp = pd.to_datetime(
-            self.dataset.CompleteTimestamp)
-        self.dataset = self.dataset.groupby("CaseID").apply(
-            lambda case: self.__time_features_dev(case))
-        self.dataset.fvt1.fillna(0, inplace=True)
-        dummy = pd.get_dummies(
-            self.dataset["ActivityID"], prefix="ActivityID", drop_first=True)
-        self.dataset = pd.concat([self.dataset, dummy], axis=1)
-        self.dataset.drop(['CompleteTimestamp', 'T2E'], axis=1, inplace=True)
-        last_step = self.dataset.drop_duplicates(
-            subset=["CaseID"], keep='last')["ActivityID"].index
+        end_list_cols = ['ActivityID_'+x for x in self.end_event_list]
         self.dataset = self.dataset.drop(
-            last_step, axis=0).reset_index(drop=True)
+            end_list_cols, axis=1, errors='ignore')
 
     def xy_split(self, scaling):
         """Spliting the dataset into X_test [and y_test if available].
@@ -345,25 +296,18 @@ class t2e:
             test_cases (int): Count of testing traces
 
         """
-        all_cases = set(self.dataset.CaseID.unique())
-        censored = set(self.censored_cases)
-        observed = list(all_cases.difference(censored))
-
-        len_train = int(train_prc * len(observed))
+        len_train = int(train_prc * len(self.observed_cases))
         len_val = int(len_train*val_prc)
-        cases_train = observed[0:len_train]
+        cases_train = self.observed_cases[0:len_train]
         cases_val = [cases_train.pop() for i in range(len_val)]
-        cases_test = observed[len_train:]
+        cases_test = self.observed_cases[len_train:]
 
-        print("\tTotal Observed:", len(observed))
-        print("\tTraining data Observed:", len(cases_train))
-        print("\tTraining data Censored:", len(censored))
         if self.censored:
-            cases_train = cases_train + list(censored)
-        print("\tTraining data to use:", len(cases_train))
+            cases_train = cases_train + self.censored_cases
 
-        print("\tValidation data:", len(cases_val))
-        print("\tTesting data   :", len(cases_test))
+        logger.info("Training   : %d", len(cases_train))
+        logger.info("Validation : %d", len(cases_val))
+        logger.info("Testing    : %d", len(cases_test))
         df_train = self.dataset.loc[self.dataset['CaseID'].isin(
             cases_train)].reset_index(drop=True)
         df_val = self.dataset.loc[self.dataset['CaseID'].isin(
@@ -383,7 +327,12 @@ class t2e:
         X_train, y_train = self.__xy_split(df_train)
         X_val, y_val = self.__xy_split(df_val)
         X_test, y_test = self.__xy_split(df_test)
-
+        X_train = np.float64(X_train)
+        y_train = np.float64(y_train)
+        X_val = np.float64(X_val)
+        y_val = np.float64(y_val)
+        X_test = np.float64(X_test)
+        y_test = np.float64(y_test)
         if self.regression is True:
             return X_train, X_test, X_val, y_train[:, 0].reshape((-1, 1)), \
                 y_test[:, 0].reshape((-1, 1)), y_val[:, 0].reshape((-1, 1)), \
@@ -415,7 +364,7 @@ class t2e:
             self.power = 1/self.root
             y_train[:, 0] = y_train[:, 0]**self.root
             y_val[:, 0] = y_val[:, 0]**self.root
-            print('Y_label has been transformed')
+            logger.info('Y_label has been transformed')
         out_path = "output/"
         check_dir(out_path)
         vb = vb
@@ -472,7 +421,7 @@ class t2e:
             )
         try:
             self.model.load_weights(out_path + 'best_model.h5')
-            print('model loaded successfully')
+            logger.info('model loaded successfully')
         except Exception:
             self.model is None
 
@@ -633,7 +582,7 @@ class t2e:
                 if self.transform is True:
                     test_results_df['T_pred'] = test_results_df['T_pred']\
                         ** self.power
-                    print("Y_label is restored")
+                    logger.info("Y_label is restored")
                 if self.resolution == 's':
                     test_results_df['error (days)'] = (
                         test_results_df['T'] - test_results_df['T_pred'])\
@@ -672,6 +621,7 @@ class t2e:
             return np.nan
 
     def __time_features(self, case):
+        # case = case.sort_values('CompleteTimestamp')
         first_index = case.index[0]
         last_index = case.index[-1]
         endtime = case["CompleteTimestamp"][last_index]
@@ -683,7 +633,7 @@ class t2e:
                         starttime).dt.total_seconds()
         case["T2E"] = endtime - case["CompleteTimestamp"]
         if self.resolution == 'd':
-            case["D2E"] = case["T2E"].dt.days
+            case["D2E"] = case["T2E"].dt.total_seconds()/86400
         elif self.resolution == 's':
             case["S2E"] = case["T2E"].dt.total_seconds()
         elif self.resolution == 'h':
@@ -691,36 +641,6 @@ class t2e:
         else:
             raise ValueError("d: day, h:hours, s:seconds are allowed as\
                 time resolution")
-        return case
-
-    def __time_features_dev(self, case):
-        case_length = len(case)
-        first_index = case.index[0]
-        last_index = case.index[-1]
-        if case.iloc[0].loc["U"] == 1:
-            endtime = case["CompleteTimestamp"][last_index]
-        else:
-            # possible cuts
-            x = np.arange(1, case_length - self.prefix)
-            # choose a cut
-            random_censor = np.random.choice(x, 1)[0]
-            for _ in range(random_censor):
-                case.drop(last_index, axis=0, inplace=True)
-                last_index = case.index[-1]
-            endtime = case["CompleteTimestamp"][last_index]
-        starttime = case["CompleteTimestamp"][first_index]
-        case["fvt1"] = case["CompleteTimestamp"].diff(
-            periods=1).dt.total_seconds()
-        case["fvt2"] = case["CompleteTimestamp"].dt.hour
-        case["fvt3"] = (case["CompleteTimestamp"] -
-                        starttime).dt.total_seconds()
-        case["T2E"] = endtime - case["CompleteTimestamp"]
-        if self.resolution == 'd':
-            case["D2E"] = case["T2E"].dt.days
-        elif self.resolution == 's':
-            case["S2E"] = case["T2E"].dt.total_seconds()
-        elif self.resolution == 'h':
-            case["H2E"] = case["T2E"].dt.total_seconds()/3600
         return case
 
     def __xy_split(self, data):
@@ -760,14 +680,16 @@ class t2e:
         elif self.resolution == 'd':
             time_idx = np.where(df.columns.str.contains("D2E"))[0]
         else:
-            print("Defualt option chosen ==> daily")
+            logger.info("Defualt option chosen ==> daily")
             time_idx = np.where(df.columns.str.contains("D2E"))[0]
 
         if len(df) <= self.prefix:
             y.append(df.values[-1, time_idx])
         else:
             y.append(df.values[self.prefix-1, time_idx])
-        y.append(df.loc[0, "U"])
+
+        # y.append(df.loc[0, "U"])
+        y.append(self.cen_dict[df['CaseID'].unique()[0]])
         y = np.hstack(np.array(y)).flatten()
         y.reshape((1, 2))
         return y
